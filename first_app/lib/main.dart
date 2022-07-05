1e71a36c336fd845ab9b32e63c35dc7a10e41f2b
HyunJay52 <hyunjungk52@gmail.com>
2022-07-05 18:24:27 +0900

    module classes

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+import 'pages/quiz.dart';
+import './result.dart';
 /*
   widgets package
   Google : Material Design
@@ -26,88 +26,74 @@ class MyFlutterApp extends StatefulWidget {
 }
 
 class MyFlutterAppState extends State<MyFlutterApp> {
+  //static
+  final _questions = const [
+    {
+      'questionText': 'what\'s yout fav colour?',
+      'answers': [
+        {'text': 'white', 'score': 5},
+        {'text': 'black', 'score': 10},
+        {'text': 'green', 'score': 15},
+        {'text': 'yellow', 'score': 20},
+      ]
+    },
+    {
+      'questionText': 'what\'s your fav animal?',
+      'answers': [
+        {'text': 'lion', 'score': 5},
+        {'text': 'elephant', 'score': 10},
+        {'text': 'rabbit', 'score': 20},
+        {'text': 'dog', 'score': 15},
+      ]
+    },
+    {
+      'questionText': 'what\'s your fav food?',
+      'answers': [
+        {'text': 'pizza', 'score': 5},
+        {'text': 'noodle', 'score': 20},
+        {'text': 'veggie', 'score': 100},
+        {'text': 'honey', 'score': 50},
+      ]
+    },
+  ];
   // base class
   //const MyFlutterApp({super.key});
   var _questionIndex = 0;
+  var _totalScore = 0;
 
   // method
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     // re-render changed properties
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
 
-    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      //_questionIndex = 0;
+      debugPrint('we have more question!');
+    } else {
+      debugPrint('no more questions...');
+    }
   }
 
   @override // draw Flutter widgets
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'what\'s yout fav colour?',
-        'answers': ['white', 'red', 'green', 'black']
-      },
-      {
-        'questionText': 'what\'s your fav animal?',
-        'answers': ['dog', 'rabbit', 'elephant', 'polar bear']
-      },
-      {
-        'questionText': 'what\'s your fav food?',
-        'answers': ['apple', 'noodle', 'pizza', 'egg']
-      },
-    ];
+    // quesiotn = []; does not work if questions is a const
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My First Flutter App'),
-        ),
-        body: Column(
-          // list
-          children: [
-            Question(
-              questions[_questionIndex]['questionText'] as String,
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer); // iterable
-            }).toList(),
-            /*  Answer(
-              _answerQuestion,
-            ),
-            Answer(
-              _answerQuestion,
-            ),
-            Answer(
-              _answerQuestion,
-            ),
-
-             ElevatedButton(
-              onPressed: answerQuestion,
-              child: const Text('Answer 1'),
-            ),
-            ElevatedButton(
-              child: const Text('Answer 2'),
-              onPressed: () => {
-                //
-                debugPrint('Answer 2 chosend !')
-              },
-            ), */
-            //_buildElevatedButton('Answer 3', () => print('answer 3 chosen !')),
-            // ElevatedButton(
-            //   child: Text('Answer 3'),
-            //   onPressed: () => print('answer 3 chosen !'),
-            // ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildElevatedButton(String text, VoidCallback? onPressed) {
-    return ElevatedButton(
-      child: Text(text),
-      onPressed: onPressed,
+          appBar: AppBar(
+            title: const Text('My First Flutter App'),
+          ),
+          body: _questionIndex < _questions.length
+              ? Quiz(
+                  answerQuestion: _answerQuestion,
+                  questionIndex: _questionIndex,
+                  questions: _questions,
+                )
+              : Result()),
     );
   }
 }
---
+++ first_app/lib/pages/quiz.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import '../question.dart';
+import '../answer.dart';
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final int questionIndex;
+  //final Function answerQuestion;
+  void Function(int score) answerQuestion;
+  //final VoidCallback answerQuestion;
+
+  Quiz({
+    required this.questions,
+    required this.answerQuestion,
+    required this.questionIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      // list
+      children: [
+        Question(
+          questions[questionIndex]['questionText'] as String,
+        ),
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(() => answerQuestion(answer['score'] as int),
+              answer['text'] as String); // iterable
+        }).toList(), // generate to create new List of answer map
+        // ... spread operator : Column is List, try inserting new List into List, in this case, use spread operator (...)
+        // ... make List to be inserted as an individual element in List
+        // https://www.geeksforgeeks.org/dart-spread-operator/
+        /*  Answer(
+              _answerQuestion,
+            ),
+            Answer(
+              _answerQuestion,
+            ),
+            Answer(
+              _answerQuestion,
+            ),
+
+             ElevatedButton(
+              onPressed: answerQuestion,
+              child: const Text('Answer 1'),
+            ),
+            ElevatedButton(
+              child: const Text('Answer 2'),
+              onPressed: () => {
+                //
+                debugPrint('Answer 2 chosend !')
+              },
+            ), */
+        //_buildElevatedButton('Answer 3', () => print('answer 3 chosen !')),
+        // ElevatedButton(
+        //   child: Text('Answer 3'),
+        //   onPressed: () => print('answer 3 chosen !'),
+        // ),
+      ],
+    );
+  }
+/*
+  Widget _buildElevatedButton(String text, VoidCallback? onPressed) {
+    return ElevatedButton(
+      child: Text(text),
+      onPressed: onPressed,
+    );
+  } */
+}
